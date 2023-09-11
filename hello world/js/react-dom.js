098b4b368b6e5c167351f5ce6aacba35b802072e
Amel <ameladdou213@gmail.com>
2023-09-11 19:13:58 +0200

    marche pas

---
+++ hello world/js/react-dom.js
@@ -0,0 +1,6 @@
+import '../index.js';
+import "./react.js";
+import ReactDOM from 'react-dom'
+import { h1 } from "./react.js";
+
+ReactDOM.render(h1, document.getElementById("container"));
---
+++ hello world/js/react.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const h1 = React.createElement("h1", null, "Hello World")
+
+export  {
+    h1
+}
